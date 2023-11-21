734e57c1e8e99f9191de0dafe004c40433d2f729
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-21 14:00:57 +0100

    inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// selezionare il contenitore del div che voglio creare
+const row = document.querySelector(".row");
+
+
+// avviare il ciclo for
+
+
+for (let i = 1; i <= 100; i++){
+
+    // creiamo il div completo
+
+    const col = 
+       '<div class="col">
+             ${i}
+        </div>
+    ';
+
+}
