cd35192a79ed0dc06cff4b955090303681519362
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-21 15:39:06 +0100

    consegna background

--- js/script.js
+++ js/script.js
@@ -10,30 +10,30 @@ for (let i = 1; i <= 100; i++){
     if(i % 5 == 0 && i % 3 == 0){
 
         const col = 
-        `<div class="col">
+        `<div class="col" id="color">
            fizzbuzz
          </div>     
 `;
      row.innerHTML += col;
-
+      document.getElementById("color").style.backgroundColor = "#f0466f";
     } else if (i % 5 == 0){
          const col = 
-         `<div class="col">
+         `<div class="col" id="color">
             buzz
           </div>     
  `;
       row.innerHTML += col;
+      document.getElementById("color").style.backgroundColor = "#ffd166";
 
      } else if (i % 3 == 0){
 
         const col = 
-        `<div class="col">
+        `<div class="col" id="color">
            fizz
          </div>     
 `;
      row.innerHTML += col;
-
-           console.log(col);
+     document.getElementById("color").style.backgroundColor = "#0cd6a1";
 
      } else {
         // creiamo il div completo
@@ -48,17 +48,4 @@ for (let i = 1; i <= 100; i++){
           row.innerHTML += col;
 
      }
-
-
-
-
-   
-
-
-
-
-   
-
-    
-
 }
