3f19509f3af384e2495f1fa107c335c2b57068c6
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-21 15:17:10 +0100

    conclusa struttura js

--- js/script.js
+++ js/script.js
@@ -7,12 +7,58 @@ const row = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++){
 
-    // creiamo il div completo
+    if(i % 5 == 0 && i % 3 == 0){
 
-    const col = 
-       '<div class="col">
-             ${i}
-        </div>
-    ';
+        const col = 
+        `<div class="col">
+           fizzbuzz
+         </div>     
+`;
+     row.innerHTML += col;
+
+    } else if (i % 5 == 0){
+         const col = 
+         `<div class="col">
+            buzz
+          </div>     
+ `;
+      row.innerHTML += col;
+
+     } else if (i % 3 == 0){
+
+        const col = 
+        `<div class="col">
+           fizz
+         </div>     
+`;
+     row.innerHTML += col;
+
+           console.log(col);
+
+     } else {
+        // creiamo il div completo
+
+        const col = 
+        `<div class="col">
+              ${i}
+         </div>
+`;
+          // aggiugiamo col al suo contenitore
+          
+          row.innerHTML += col;
+
+     }
+
+
+
+
+   
+
+
+
+
+   
+
+    
 
 }
